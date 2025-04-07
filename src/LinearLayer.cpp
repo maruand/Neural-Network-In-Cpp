e7f712617d3b2bc04dc51796431f81dca59de305
marcus <marua@chalmers.se>
2025-04-07 08:12:09 +0200

    implemented initWeights and initBias

--- src/LinearLayer.cpp
+++ src/LinearLayer.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <cmath>
+#include <cstdlib>
 #include <numeric>
+#include <random>
+
 
 class LinearLayer {
     public:
@@ -15,33 +18,49 @@ class LinearLayer {
     private:
         int nInputs;
         int nOutputs;
-        float* weights;
-        float* bias;
-        float* gradWeights;
-        float* gradBias;
         float* input;
         float* output;
+        std::vector<float> weights;
+        std::vector<float> bias;
+        std::vector<float> gradWeights;
+        std::vector<float> gradBias;
+
     
 };
 
 LinearLayer::LinearLayer(int nInputs, int nOutputs) {
     this->nInputs = nInputs;
     this->nOutputs = nOutputs;
-    weights = new float[nInputs * nOutputs]; // Dynamically allocate memory for weights
-    bias = new float[nOutputs]; // Dynamically allocate memory for bias
-    gradWeights = new float[nInputs * nOutputs]; // Dynamically allocate memory for gradient of weights
-    gradBias = new float[nOutputs]; // Dynamically allocate memory for gradient of bias
     input = new float[nInputs]; // Dynamically allocate memory for input
     output = new float[nOutputs]; // Dynamically allocate memory for output
 
 };
 LinearLayer::~LinearLayer() {
-    delete[] weights; // Free allocated memory for weights
-    delete[] bias; // Free allocated memory for bias
-    delete[] gradWeights; // Free allocated memory for gradient of weights
-    delete[] gradBias; // Free allocated memory for gradient of bias
     delete[] input; // Free allocated memory for input
     delete[] output; // Free allocated memory for output
+};
+
+void LinearLayer::initWeights(){
+    std::random_device rd{};
+    std::mt19937 gen{rd()};
+
+    std::normal_distribution d{0.0, sqrt(nInputs)}; // Normal distribution with mean=0, std=sqrt(nInputs)
+
+    auto random_float = [&d, &gen]{return d(gen);}; // Function to draw a random float from the normal dist
+
+    // Populate the wegihts matrix with random floats
+    for (int i = 0; i < nInputs; i++ ){
+        for (int j = 0; j < nOutputs; j++){
+            weights[i,j] = random_float();
+        };
+        
+    };
+};
+
+void LinearLayer::initBias(){
+    for (int i; i < nOutputs; i++){
+        bias[i] = 0;
+    };
 };
 void LinearLayer::forward(float* input){
 
