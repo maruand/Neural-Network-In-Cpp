6e864d3eddd407e46df9c67e07ca73f6f86df52d
marcus <marua@chalmers.se>
2025-04-26 13:11:55 +0200

    pain

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -3,9 +3,23 @@
 #include <string>
 #include <vector>
 #include <Eigen/Dense>
+#include <typeinfo>
+#include <csignal>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+
+
+int count_rows(const std::string& file_name) {
+    std::ifstream file(file_name);
+    std::string line;
+    int row_count = 0;
+    while (getline(file, line)) {
+        ++row_count;
+    }
+    return row_count;
+};
+
 
 std::vector<VectorXd> readFile(std::string file_name, bool hasHeader = true){
 
@@ -20,8 +34,9 @@ std::vector<VectorXd> readFile(std::string file_name, bool hasHeader = true){
     float val;
     float val_;
     int fileLine = 0;
+    int num_rows = count_rows(file_name);
+    VectorXd target(num_rows);
 
-  
     int rowIdx = 0;
 
     while(std::getline(file, line))
@@ -33,25 +48,48 @@ std::vector<VectorXd> readFile(std::string file_name, bool hasHeader = true){
         {
             if(ss1.peek() == ',') ss1.ignore();
             colLength++;
+        };
+
+        if (hasHeader && fileLine==0){
+            fileLine++;
+            continue;
+        }
+        if(colLength <= 1){
+            std::cout << "Omittig line: " << fileLine << std::endl;
+            continue;
+        };
+        
+        
+        int colIdx = 0;
+        VectorXd col(colLength-1);
+
+        if (fileLine == 2274) {
+            raise(SIGTRAP); // Trigger a breakpoint for the debugger
+        }
+
+        while (ss >> val) {   
+            if (colIdx < colLength - 1) {
+                col(colIdx) = val;
+                if (ss.peek() == ',') ss.ignore();
+                colIdx++;
+            } else {
+                // Check if the last value is an integer
+                if (std::floor(val) != val) {
+                    //std::cout << "Skipping line " << fileLine << " due to non-integer target value: " << val << std::endl;
+                    fileLine++;
+                    continue; // Skip this line
+                }
+                target(fileLine) = val; // Add the last value to the target vector
+            }
         }
         
-
-        int colIdx = 0;
-        VectorXd col(colLength);
-        while (ss >> val)
-        {
-            col(colIdx) = val;
-            if(ss.peek() == ',') ss.ignore();
-            colIdx++;
-        }
-
         if(!hasHeader || fileLine > 0){
             rowIdx++;
             result.push_back(col);
         }
         fileLine++;
     }
-
+    result.push_back(target);
     file.close();
 
     return result;
@@ -66,7 +104,7 @@ int main()
     /* code */
     std::vector<VectorXd> test = readFile("/Users/marcusandersson/Desktop/neural_network/data/processed_data.csv", true); // Set to 'false' if no header
 
-    std::cout << test.at(0) << std::endl;
+    std::cout << test.back() << std::endl;
     return 0;
 }
 
